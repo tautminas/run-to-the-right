203f3b5a03da0b4ada4cc6e47f37c73bc5410cfb
Tautminas Cibulskis <tautminas@yahoo.com>
2024-01-11 00:58:00 +0200

    Activate option on space

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -1,6 +1,10 @@
 import * as Phaser from "phaser";
 
 export default class MenuScene extends Phaser.Scene {
+  private selectedItemIndex: number = 0;
+  private menuItems: Phaser.GameObjects.Text[] = [];
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+
   constructor() {
     super("MenuScene");
   }
@@ -38,7 +42,52 @@ export default class MenuScene extends Phaser.Scene {
       .setOrigin(0.5);
     exitText.setInteractive();
     exitText.on("pointerdown", () => console.log("Exit"));
+
+    this.menuItems = [playText, scoreText, exitText];
+    this.selectMenuItem(this.selectedItemIndex);
   }
 
-  update() {}
+  update() {
+    if (this.input && this.input.keyboard) {
+      this.cursors = this.input.keyboard.createCursorKeys();
+    } else {
+      console.error("Input or keyboard is not available");
+    }
+    const isDownJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.down);
+    const isUpJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.up);
+    const isSpaceJustPressed = Phaser.Input.Keyboard.JustDown(
+      this.cursors.space
+    );
+
+    if (isDownJustPressed) {
+      this.selectedItemIndex = Phaser.Math.Wrap(
+        this.selectedItemIndex + 1,
+        0,
+        this.menuItems.length
+      );
+      this.selectMenuItem(this.selectedItemIndex);
+    } else if (isUpJustPressed) {
+      this.selectedItemIndex = Phaser.Math.Wrap(
+        this.selectedItemIndex - 1,
+        0,
+        this.menuItems.length
+      );
+      this.selectMenuItem(this.selectedItemIndex);
+    }
+
+    if (isSpaceJustPressed) {
+      const selectedItem = this.menuItems[this.selectedItemIndex];
+      selectedItem.emit("pointerdown");
+    }
+  }
+
+  selectMenuItem(index: number) {
+    this.menuItems.forEach((item, i) => {
+      if (i === index) {
+        item.setColor("#ff0000");
+      } else {
+        item.setColor("#000000");
+      }
+    });
+  }
 }

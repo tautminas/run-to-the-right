e9f356555ddd8a4a048ab0dd42cd41470edd3c58
Tautminas Cibulskis <tautminas@yahoo.com>
2024-01-21 02:46:19 +0200

    Add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite';
-import replace from '@rollup/plugin-replace';
+import { defineConfig } from "vite";
+import replace from "@rollup/plugin-replace";
 
 export default defineConfig({
+  base: "/run-to-the-right/",
   build: {
     rollupOptions: {
       plugins: [
         //  Toggle the booleans here to enable / disable Phaser 3 features:
         replace({
-          'typeof CANVAS_RENDERER': "'true'",
-          'typeof WEBGL_RENDERER': "'true'",
-          'typeof EXPERIMENTAL': "'true'",
-          'typeof PLUGIN_CAMERA3D': "'false'",
-          'typeof PLUGIN_FBINSTANT': "'false'",
-          'typeof FEATURE_SOUND': "'true'"
-        })
-      ]
-    }
-  }
+          "typeof CANVAS_RENDERER": "'true'",
+          "typeof WEBGL_RENDERER": "'true'",
+          "typeof EXPERIMENTAL": "'true'",
+          "typeof PLUGIN_CAMERA3D": "'false'",
+          "typeof PLUGIN_FBINSTANT": "'false'",
+          "typeof FEATURE_SOUND": "'true'",
+        }),
+      ],
+    },
+  },
 });
